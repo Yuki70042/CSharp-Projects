c904aec9267e08c469f8d9829ccf7cb983d2d83d
Yuki70042 <lizlink2004@gmail.com>
2024-07-21 20:59:14 +0200

    First test

--- 4. Memory Game/4. Memory Game/obj/Debug/net8.0-windows/4. Memory Game.AssemblyInfo.cs
+++ 4. Memory Game/4. Memory Game/obj/Debug/net8.0-windows/4. Memory Game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4. Memory Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3d1bf64a71c4bb45169deb47c1b7fd54218955e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8deed874699f667f253262b7f5e44cb3ae4a560f")]
 [assembly: System.Reflection.AssemblyProductAttribute("4. Memory Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4. Memory Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
