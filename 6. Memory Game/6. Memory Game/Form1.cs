00da42c49f504d2d36d645d4915f767fbe4773f8
Yuki70042 <lizlink2004@gmail.com>
2024-07-29 18:15:04 +0200

    create a base for the game

--- 6. Memory Game/6. Memory Game/Form1.cs
+++ 6. Memory Game/6. Memory Game/Form1.cs
@@ -16,5 +16,10 @@ namespace _6.Memory_Game
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
