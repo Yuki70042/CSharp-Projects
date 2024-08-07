7a570d4634135a20a5cdc8d00723e837f83a0fd0
Yuki70042 <lizlink2004@gmail.com>
2024-08-07 20:53:39 +0200

    Add the clear function and the event to change the game mode

--- 7.Tic-Tac-Toe/7.Tic-Tac-Toe/Form1.Designer.cs
+++ 7.Tic-Tac-Toe/7.Tic-Tac-Toe/Form1.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.button10 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
@@ -38,10 +37,11 @@
             this.button5 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
             this.RestartButton = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.ChangeMode = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
@@ -73,14 +73,14 @@
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
-            this.tableLayoutPanel2.Controls.Add(this.button10, 2, 2);
-            this.tableLayoutPanel2.Controls.Add(this.button9, 1, 2);
-            this.tableLayoutPanel2.Controls.Add(this.button8, 0, 2);
-            this.tableLayoutPanel2.Controls.Add(this.button7, 2, 1);
-            this.tableLayoutPanel2.Controls.Add(this.button6, 1, 1);
-            this.tableLayoutPanel2.Controls.Add(this.button5, 0, 1);
-            this.tableLayoutPanel2.Controls.Add(this.button4, 2, 0);
-            this.tableLayoutPanel2.Controls.Add(this.button3, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button9, 2, 2);
+            this.tableLayoutPanel2.Controls.Add(this.button8, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.button7, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.button6, 2, 1);
+            this.tableLayoutPanel2.Controls.Add(this.button5, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.button4, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.button3, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button2, 1, 0);
             this.tableLayoutPanel2.Controls.Add(this.button1, 0, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(6, 6);
@@ -92,29 +92,16 @@
             this.tableLayoutPanel2.Size = new System.Drawing.Size(460, 424);
             this.tableLayoutPanel2.TabIndex = 0;
             // 
-            // button10
-            // 
-            this.button10.AutoSize = true;
-            this.button10.BackColor = System.Drawing.Color.MintCream;
-            this.button10.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button10.Font = new System.Drawing.Font("Webdings", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.button10.Location = new System.Drawing.Point(309, 285);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(148, 136);
-            this.button10.TabIndex = 8;
-            this.button10.UseVisualStyleBackColor = false;
-            this.button10.Click += new System.EventHandler(this.button_Click);
-            // 
             // button9
             // 
             this.button9.AutoSize = true;
             this.button9.BackColor = System.Drawing.Color.MintCream;
             this.button9.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button9.Font = new System.Drawing.Font("Webdings", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.button9.Location = new System.Drawing.Point(156, 285);
+            this.button9.Location = new System.Drawing.Point(309, 285);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(147, 136);
-            this.button9.TabIndex = 7;
+            this.button9.Size = new System.Drawing.Size(148, 136);
+            this.button9.TabIndex = 8;
             this.button9.UseVisualStyleBackColor = false;
             this.button9.Click += new System.EventHandler(this.button_Click);
             // 
@@ -124,10 +111,10 @@
             this.button8.BackColor = System.Drawing.Color.MintCream;
             this.button8.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button8.Font = new System.Drawing.Font("Webdings", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.button8.Location = new System.Drawing.Point(3, 285);
+            this.button8.Location = new System.Drawing.Point(156, 285);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(147, 136);
-            this.button8.TabIndex = 6;
+            this.button8.TabIndex = 7;
             this.button8.UseVisualStyleBackColor = false;
             this.button8.Click += new System.EventHandler(this.button_Click);
             // 
@@ -137,10 +124,10 @@
             this.button7.BackColor = System.Drawing.Color.MintCream;
             this.button7.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button7.Font = new System.Drawing.Font("Webdings", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.button7.Location = new System.Drawing.Point(309, 144);
+            this.button7.Location = new System.Drawing.Point(3, 285);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(148, 135);
-            this.button7.TabIndex = 5;
+            this.button7.Size = new System.Drawing.Size(147, 136);
+            this.button7.TabIndex = 6;
             this.button7.UseVisualStyleBackColor = false;
             this.button7.Click += new System.EventHandler(this.button_Click);
             // 
@@ -150,10 +137,10 @@
             this.button6.BackColor = System.Drawing.Color.MintCream;
             this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button6.Font = new System.Drawing.Font("Webdings", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.button6.Location = new System.Drawing.Point(156, 144);
+            this.button6.Location = new System.Drawing.Point(309, 144);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(147, 135);
-            this.button6.TabIndex = 4;
+            this.button6.Size = new System.Drawing.Size(148, 135);
+            this.button6.TabIndex = 5;
             this.button6.UseVisualStyleBackColor = false;
             this.button6.Click += new System.EventHandler(this.button_Click);
             // 
@@ -163,10 +150,10 @@
             this.button5.BackColor = System.Drawing.Color.MintCream;
             this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button5.Font = new System.Drawing.Font("Webdings", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.button5.Location = new System.Drawing.Point(3, 144);
+            this.button5.Location = new System.Drawing.Point(156, 144);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(147, 135);
-            this.button5.TabIndex = 3;
+            this.button5.TabIndex = 4;
             this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button_Click);
             // 
@@ -176,10 +163,10 @@
             this.button4.BackColor = System.Drawing.Color.MintCream;
             this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button4.Font = new System.Drawing.Font("Webdings", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.button4.Location = new System.Drawing.Point(309, 3);
+            this.button4.Location = new System.Drawing.Point(3, 144);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(148, 135);
-            this.button4.TabIndex = 2;
+            this.button4.Size = new System.Drawing.Size(147, 135);
+            this.button4.TabIndex = 3;
             this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button_Click);
             // 
@@ -189,12 +176,25 @@
             this.button3.BackColor = System.Drawing.Color.MintCream;
             this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button3.Font = new System.Drawing.Font("Webdings", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
-            this.button3.Location = new System.Drawing.Point(156, 3);
+            this.button3.Location = new System.Drawing.Point(309, 3);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(147, 135);
-            this.button3.TabIndex = 1;
+            this.button3.Size = new System.Drawing.Size(148, 135);
+            this.button3.TabIndex = 2;
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.BackColor = System.Drawing.Color.MintCream;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Font = new System.Drawing.Font("Webdings", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button2.Location = new System.Drawing.Point(156, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 135);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button_Click);
             // 
             // button1
             // 
@@ -216,7 +216,7 @@
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.26087F));
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.69565F));
             this.tableLayoutPanel3.Controls.Add(this.RestartButton, 2, 0);
-            this.tableLayoutPanel3.Controls.Add(this.button2, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.ChangeMode, 0, 0);
             this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel3.Location = new System.Drawing.Point(6, 439);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
@@ -237,16 +237,17 @@
             this.RestartButton.UseVisualStyleBackColor = false;
             this.RestartButton.Click += new System.EventHandler(this.RestartButton_Click);
             // 
-            // button2
+            // ChangeMode
             // 
-            this.button2.BackColor = System.Drawing.Color.Wheat;
-            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button2.Location = new System.Drawing.Point(3, 3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(146, 65);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Play With Player2\r\n";
-            this.button2.UseVisualStyleBackColor = false;
+            this.ChangeMode.BackColor = System.Drawing.Color.Wheat;
+            this.ChangeMode.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ChangeMode.Location = new System.Drawing.Point(3, 3);
+            this.ChangeMode.Name = "ChangeMode";
+            this.ChangeMode.Size = new System.Drawing.Size(146, 65);
+            this.ChangeMode.TabIndex = 1;
+            this.ChangeMode.Text = "Play with Player 2";
+            this.ChangeMode.UseVisualStyleBackColor = false;
+            this.ChangeMode.Click += new System.EventHandler(this.ChangeMode_Click);
             // 
             // Form1
             // 
@@ -274,7 +275,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
         private System.Windows.Forms.Button RestartButton;
-        private System.Windows.Forms.Button button10;
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button7;
@@ -283,6 +283,7 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button ChangeMode;
     }
 }
 
--- 7.Tic-Tac-Toe/7.Tic-Tac-Toe/Form1.cs
+++ 7.Tic-Tac-Toe/7.Tic-Tac-Toe/Form1.cs
@@ -15,13 +15,14 @@ namespace _7.Tic_Tac_Toe
         public Form1()
         {
 
-            InitializeComponent();
+            InitializeComponent();          
             MessageBox.Show("Hi Player, I'm zero the new IA." , "Zero");
             MessageBox.Show(" Can you beat me? You are the circle...", "Good Luck");
         }
 
         private string currentPlayer = "O";
         string symbol = "n";
+        bool AIMode = true;
 
 
         private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
@@ -62,9 +63,30 @@ namespace _7.Tic_Tac_Toe
 
         }
 
+
+
+        private void ChangeMode_Click(object sender, EventArgs e)
+            // Change in the two player mode if currently in IA mode and reverse
+        {
+            
+            if (ChangeMode.Text == "Play with Player 2")
+            {
+                ChangeMode.Text = "Play with AI";
+                AIMode = false;
+                ClearBoard();
+            }
+            else
+            {
+                ChangeMode.Text = "Play with Player 2";
+                ClearBoard();
+            }
+        }
+
         private void RestartButton_Click(object sender, EventArgs e)
+        // When the restart button is click, clear the gameboard
         {
 
+            ClearBoard();
         }
 
 
@@ -91,5 +113,23 @@ namespace _7.Tic_Tac_Toe
         {
 
         }
+
+        private void ClearBoard()
+        //A method to clear the game board
+        {           
+            foreach (Control control in tableLayoutPanel2.Controls)
+            {
+                // Check if Control is a button
+                if (control is Button button)
+                {
+                    // Erase all the content                     
+                    button.Text = string.Empty;
+                    
+                }
+            }
+        }
+
+
+
     }
 }
