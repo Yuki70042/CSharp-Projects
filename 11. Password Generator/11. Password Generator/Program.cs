fd28e30db84bdf047318dc71e7fb4eaf49acfc63
Yuki70042 <82507110+Yuki70042@users.noreply.github.com>
2024-08-27 09:40:54 +0200

    Start Password Generator project (Console)

---
+++ 11. Password Generator/11. Password Generator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Program
+{
+
+    class Calculator
+    {
+        
+        static void Main(string[] args)
+        {
+            string password = "mdp";
+            Console.Write($"Your new password is: '{password}'");
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
