54239bc115e2edd641fe5f2f767d906aabfbd930
Yuki70042 <82507110+Yuki70042@users.noreply.github.com>
2024-08-27 11:41:29 +0200

    Add Generate characters Method

--- 11. Password Generator/11. Password Generator/Program.cs
+++ 11. Password Generator/11. Password Generator/Program.cs
@@ -1,20 +1,82 @@
 ﻿using System;
 
-
 namespace Program
 {
 
     class Calculator
     {
         
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            string password = "mdp";
-            Console.Write($"Your new password is: '{password}'");
+            List<string> password = new List<string>();
+
+            Console.WriteLine($"Your new password is: '{password}'");
+            CreatePassword(password, 5,5,5);    
+            foreach (string character in password)
+            {
+                Console.Write(character);
+            }
+        }
+
+
+
+
+        public static void CreatePassword(List<string>password, int letter, int number, int special)
+        {
+            RandomLetter(password,letter);
+            RandomNumber(password, number);
+            RandomSpecialCharacter(password, number);
+        }
+
+
+
+        public static void RandomLetter(List<string> listCharacters, int nbr)
+            // Add random letter to the list (uppercase and lowercase)
+        {
+            Random rand = new Random();
+            string letterlist = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            for (int i = 0; i < nbr; i++)
+            {
+                char randomletter = letterlist[rand.Next(0, 52)];
+                listCharacters.Add(randomletter.ToString());
+            }
+        }
+
+        public static void RandomSpecialCharacter(List<string> listCharacters, int nbr)
+            // Add Random Specials characters to the list
+        {
+            Random rand = new Random();
+            string speciallist = "&(-_)*/:,.~#'@{}[]";
+
+            for (int i = 0; i < nbr; i++)
+            {
+                char randomcharacter = speciallist[rand.Next(0, 18)];
+                listCharacters.Add(randomcharacter.ToString());
+            }
+        }
+
+
+        public static void RandomNumber(List<string> listCharacters, int nbr)
+            // Add random number to the list
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < nbr; i++)
+            {
+                int randomnumber = rand.Next(0, 10);
+                listCharacters.Add(randomnumber.ToString());
+            }
+        }
+
+
+        private static void MixPassword(List<string> password)
+        {
 
         }
-    }
 
+
+    }
 }
 
 
