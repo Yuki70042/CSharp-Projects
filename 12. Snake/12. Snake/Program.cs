1dbbc85962b94ce753e1405c2d3315748dcd9d28
Yuki70042 <lizlink2004@gmail.com>
2024-09-02 18:22:59 +0200

    Start new Project : Snake game

---
+++ 12. Snake/12. Snake/Program.cs
@@ -0,0 +1,25 @@
+﻿using Raylib_CsLo;
+public static class Program
+{
+    public static Task Main(string[] args)
+    {
+        Raylib.InitWindow(800, 600, "Snake");
+        Raylib.SetTargetFPS(60);
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+
+            Raylib.ClearBackground(Raylib.LIGHTGRAY);
+            
+            Raylib.DrawText("Bienvenue sur ce jeu de snake !", 10, 10, 20, Raylib.BLACK);
+
+            Raylib.EndDrawing();
+        }
+        
+
+        Raylib.CloseWindow();
+        return Task.CompletedTask;
+    }
+    
+}
